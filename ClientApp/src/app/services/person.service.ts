2dc6b21904e631d4d6f84e074b6ee24c5c42d87b
Carlos Pavajeau <cantte098@gmail.com>
2020-03-24 10:15:58 -0500

    Add new methods in person.service

--- ClientApp/src/app/services/person.service.ts
+++ ClientApp/src/app/services/person.service.ts
@@ -1,12 +1,47 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Inject } from '@angular/core';
 import { Person } from "../models/person";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { HandleHttpErrorService } from '../@base/handle-http-error.service';
+import { Observable } from 'rxjs';
+import { tap, catchError } from 'rxjs/operators';
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type' : 'application/json',
+    'Authorization' : 'my-auth-token'  
+  })
+};
 
 @Injectable({
   providedIn: 'root'
 })
 export class PersonService {
 
-  constructor() { }
+  private baseUrl: string;
+
+  constructor(
+    private http: HttpClient,
+    @Inject('BASE_URL') baseUrl: string,
+    private handleHttpError: HandleHttpErrorService
+  ) { 
+    this.baseUrl = baseUrl;
+  }
+
+  post(person: Person): Observable<Person> {
+    return this.http.post<Person>(this.baseUrl + 'api/People/Add', person, httpOptions)
+      .pipe(
+        tap(_ => this.handleHttpError.log('Datos guardados')),
+        catchError(this.handleHttpError.handleError<Person>('savePerson', null))
+      );
+  }
+
+  get(): Observable<Person[]> {
+    return this.http.get<Person[]>(this.baseUrl + 'api/People/People')
+      .pipe(
+        tap(_ => this.handleHttpError.log('Datos recividos')),
+        catchError(this.handleHttpError.handleError<Person[]>('getPeople', []))
+      );
+  }
 
   savePerson(person: Person): void
   {
