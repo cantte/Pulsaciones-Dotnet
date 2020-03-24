bc3fbda00b25ac127ea1afe605f6ca17296e281a
Carlos Pavajeau <cantte098@gmail.com>
2020-03-24 10:14:02 -0500

    Add models

---
+++ Models/Person.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pulsaciones_dotnetV2.Models
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string PersonId { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Sex { get; set; }
+        public float Pulsations 
+        {
+            get
+            {
+                return (Sex == "F") ? ((210 - Age) / 10) : ((220 - Age) / 10);
+            }
+        }
+    }
+}
---
+++ Models/ViewModels/PersonViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pulsaciones_dotnetV2.Models.ViewModels
+{
+    public class PersonViewModel
+    {
+        public int Id { get; set; }
+        public string PersonId { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Sex { get; set; }
+        public float Pulsations { get; set; }
+    }
+}
