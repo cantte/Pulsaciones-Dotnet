5f0078d540cebb27a8dd768602b0a2b1fbee284b
Carlos Pavajeau <cantte098@gmail.com>
2020-03-24 17:20:31 -0500

    Add new models

---
+++ Models/Response/ServerResponse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pulsaciones_dotnetV2.Models.Response
+{
+    public class ServerResponse
+    {
+        public bool Success { get; set; }
+        public string Message { get; set; }
+    }
+}
