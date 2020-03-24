8865f6d149154109c1c66ac04508c7f7969882fc
Carlos Pavajeau <cantte098@gmail.com>
2020-03-23 22:46:18 -0500

    Change nav menu and add app-routing module

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -6,12 +6,19 @@ import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
-import { HomeComponent } from './home/home.component';
+import { HomeComponent } from './components/home/home.component';
 import { CounterComponent } from './counter/counter.component';
 import { FetchDataComponent } from './fetch-data/fetch-data.component';
 import { ApiAuthorizationModule } from 'src/api-authorization/api-authorization.module';
 import { AuthorizeGuard } from 'src/api-authorization/authorize.guard';
 import { AuthorizeInterceptor } from 'src/api-authorization/authorize.interceptor';
+import { PeopleComponent } from './components/people/people.component';
+import { PeopleHistoryComponent } from './components/people-history/people-history.component';
+import { PersonDetailComponent } from './components/person-detail/person-detail.component';
+import { PersonSearchComponent } from './components/person-search/person-search.component';
+import { PersonResgisterComponent } from './components/person-resgister/person-resgister.component';
+import { AppRoutingModule } from './app-routing.module';
+import { PersonService } from './services/person.service';
 
 @NgModule({
   declarations: [
@@ -19,21 +26,23 @@ import { AuthorizeInterceptor } from 'src/api-authorization/authorize.intercepto
     NavMenuComponent,
     HomeComponent,
     CounterComponent,
-    FetchDataComponent
+    FetchDataComponent,
+    PeopleComponent,
+    PeopleHistoryComponent,
+    PersonDetailComponent,
+    PersonSearchComponent,
+    PersonResgisterComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
     HttpClientModule,
     FormsModule,
     ApiAuthorizationModule,
-    RouterModule.forRoot([
-      { path: '', component: HomeComponent, pathMatch: 'full' },
-      { path: 'counter', component: CounterComponent },
-      { path: 'fetch-data', component: FetchDataComponent, canActivate: [AuthorizeGuard] },
-    ])
+    AppRoutingModule
   ],
   providers: [
-    { provide: HTTP_INTERCEPTORS, useClass: AuthorizeInterceptor, multi: true }
+    { provide: HTTP_INTERCEPTORS, useClass: AuthorizeInterceptor, multi: true },
+    PersonService
   ],
   bootstrap: [AppComponent]
 })
