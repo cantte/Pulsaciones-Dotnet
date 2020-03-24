9f9326e3ce19c30ee781478194d4a737c17b0ffe
Carlos Pavajeau <cantte098@gmail.com>
2020-03-24 10:16:21 -0500

    Add new table People

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -12,6 +12,8 @@ namespace Pulsaciones_dotnetV2.Data
 {
     public class ApplicationDbContext : ApiAuthorizationDbContext<ApplicationUser>
     {
+        public DbSet<Person> People { get; set; }
+
         public ApplicationDbContext(
             DbContextOptions options,
             IOptions<OperationalStoreOptions> operationalStoreOptions) : base(options, operationalStoreOptions)
