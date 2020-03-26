589e7be0685b4cb3001298037ad7afe18364b473
Carlos Pavajeau <cantte098@gmail.com>
2020-03-26 17:36:10 -0500

    BackEnd: Add PersonInputModel for receive input data

---
+++ Models/InputModels/PersonInputModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pulsaciones_dotnetV2.Models.InputModels
+{
+    public class PersonInputModel
+    {
+        public string PersonId { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Sex { get; set; }
+    }
+}
