48c918e1285413e09111cd3913a55faa059d61fa
Carlos Pavajeau <cantte098@gmail.com>
2020-03-25 21:41:48 -0500

    FrontEnd: Some improvements

--- ClientApp/src/app/components/person-detail/person-detail.component.ts
+++ ClientApp/src/app/components/person-detail/person-detail.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Person } from 'src/app/models/person';
 import { PersonService } from 'src/app/services/person.service';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Location } from "@angular/common";
 
 @Component({
@@ -16,11 +16,16 @@ export class PersonDetailComponent implements OnInit {
   constructor(
     private personService: PersonService,
     private route: ActivatedRoute,
-    private location: Location
+    private location: Location,
+    private router: Router
     ) { }
 
   ngOnInit(): void {
     this.getPerson();
+
+    this.router.events.subscribe((val: any) => {
+      this.getPerson();
+    });
   }
 
   getPerson(): void
