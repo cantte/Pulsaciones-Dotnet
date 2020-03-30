d674361fac6422be334eda66b542a534bb56945a
Carlos Pavajeau <cantte098@gmail.com>
2020-03-30 10:17:20 -0500

    Add EditModels

---
+++ Models/EditModels/PersonEditModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pulsaciones_dotnetV2.Models.EditModels
+{
+    public class PersonEditModel
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Sex { get; set; }
+    }
+}
