0a97a3c69c3ecae00193a0fdc26f25e5e7d9a773
Carlos Pavajeau <cantte098@gmail.com>
2020-06-17 12:12:39 -0500

    Some improvements

--- ClientApp/src/app/components/person-resgister/person-resgister.component.ts
+++ ClientApp/src/app/components/person-resgister/person-resgister.component.ts
@@ -1,134 +1,123 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators, FormBuilder, AbstractControl } from "@angular/forms";
+import { FormGroup, FormControl, Validators, FormBuilder, AbstractControl } from '@angular/forms';
 import { Person } from 'src/app/models/person';
 import { PersonService } from 'src/app/services/person.service';
 import { ServerResponse } from 'src/app/@base/server-response';
 import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
-  selector: 'app-person-resgister',
-  templateUrl: './person-resgister.component.html',
-  styleUrls: ['./person-resgister.component.css']
+	selector: 'app-person-resgister',
+	templateUrl: './person-resgister.component.html',
+	styleUrls: [ './person-resgister.component.css' ]
 })
 export class PersonResgisterComponent implements OnInit {
+	personForm: FormGroup;
+	person: Person = new Person();
+	invalidForm: boolean;
+	updateForm: boolean = false;
+	serverResponse: ServerResponse = null;
+	pulsations: number = null;
+	title: string;
 
-  personForm: FormGroup;
-  person: Person = new Person();
-  invalidForm: boolean;
-  updateForm: boolean = false;
-  serverResponse: ServerResponse = null;
-  pulsations: number = null;
-  title: string;
+	public get controls() {
+		return this.personForm.controls;
+	}
 
-  
-  public get controls() {
-    return this.personForm.controls;
-  }
-  
+	constructor(
+		private personService: PersonService,
+		private route: ActivatedRoute,
+		private router: Router,
+		private formBuilder: FormBuilder
+	) {}
 
-  constructor(
-    private personService: PersonService,
-    private route: ActivatedRoute,
-    private router: Router,
-    private formBuilder: FormBuilder
-  ) { }
+	ngOnInit(): void {
+		this.initForm();
+		this.setFormType();
 
-  ngOnInit(): void {
-    this.initForm();
-    this.setFormType();
-    
-    if (this.updateForm) {
-      this.controls['personId'].disable();
-      this.loadPerson();
-    } else {
-      const id = this.route.snapshot.paramMap.get('id');
-      if (id) {
-        this.controls['personId'].disable();
-        this.controls['personId'].setValue(id);
-      }
-    }
-  }
+		if (this.updateForm) {
+			this.controls['personId'].disable();
+			this.loadPerson();
+		} else {
+			const id = this.route.snapshot.paramMap.get('id');
+			if (id) {
+				this.controls['personId'].disable();
+				this.controls['personId'].setValue(id);
+			}
+		}
+	}
 
-  initForm() {
-    this.personForm = this.formBuilder.group({
-      personId: ['', [Validators.required, Validators.minLength(5), Validators.pattern('[0-9]+')]],
-      personName: ['', [Validators.required, Validators.minLength(5)]],
-      personAge: ['', [Validators.required, Validators.max(100), Validators.min(1)]],
-      personSex: ['', [Validators.required]]
-    });
-  }
+	initForm() {
+		this.personForm = this.formBuilder.group({
+			personId: [ '', [ Validators.required, Validators.minLength(5), Validators.pattern('[0-9]+') ] ],
+			personName: [ '', [ Validators.required, Validators.minLength(5) ] ],
+			personAge: [ '', [ Validators.required, Validators.max(100), Validators.min(1) ] ],
+			personSex: [ '', [ Validators.required ] ]
+		});
+	}
 
-  setFormType() {
-    const urlPath = this.route.snapshot.url[0].path;
-    this.title = `Form of ${urlPath} person`;
-    if (urlPath == 'edit') {
-      this.updateForm = true;
-    }
-  }
+	setFormType() {
+		const urlPath = this.route.snapshot.url[0].path;
+		this.title = `Form of ${urlPath} person`;
+		if (urlPath == 'edit') {
+			this.updateForm = true;
+		}
+	}
 
-  loadPerson() {
-    const id = this.route.snapshot.paramMap.get('id');
-    this.personService.getPerson(id).subscribe(p => {
-      if (p != null) {
-        this.pulsations = p.pulsations;
-        this.controls['personId'].setValue(p.personId);
-        this.controls['personName'].setValue(p.name);
-        this.controls['personAge'].setValue(p.age);
-        this.controls['personSex'].setValue(p.sex);
-      }
-    });
-  }
+	loadPerson() {
+		const id = this.route.snapshot.paramMap.get('id');
+		this.personService.getPerson(id).subscribe((p) => {
+			if (p != null) {
+				this.pulsations = p.pulsations;
+				this.controls['personId'].setValue(p.personId);
+				this.controls['personName'].setValue(p.name);
+				this.controls['personAge'].setValue(p.age);
+				this.controls['personSex'].setValue(p.sex);
+			}
+		});
+	}
 
-  onSubmit() {
-    if (this.personForm.valid) {
-      this.person.personId = this.controls['personId'].value;
-      this.person.name = this.controls['personName'].value;
-      this.person.age = +this.controls['personAge'].value;
-      this.person.sex = this.controls['personSex'].value;
+	onSubmit() {
+		if (this.personForm.valid) {
+			this.person.personId = this.controls['personId'].value;
+			this.person.name = this.controls['personName'].value;
+			this.person.age = +this.controls['personAge'].value;
+			this.person.sex = this.controls['personSex'].value;
 
-      if (this.updateForm) {
-        this.personService.put(this.person).subscribe(serverResponse => {
-          this.handleServerResponse(serverResponse);
-        });
-      }
-      else {
-        this.personService.post(this.person).subscribe(serverResponse => {
-          this.handleServerResponse(serverResponse);
-        });
-      }
-    }
-    else this.invalidForm = true;
-  }
+			if (this.updateForm) {
+				this.personService.put(this.person).subscribe((serverResponse) => {
+					this.handleServerResponse(serverResponse);
+				});
+			} else {
+				this.personService.post(this.person).subscribe((person) => {
+					this.handleServerResponse(person);
+				});
+			}
+		} else this.invalidForm = true;
+	}
 
-  handleServerResponse(serverResponse: ServerResponse) {
-    this.serverResponse = serverResponse;
-    setTimeout(() => {
-      this.serverResponse = null;
-      this.person = new Person();
-      if (this.updateForm) {
-        if (serverResponse.success)
-          this.router.navigateByUrl(`person/${this.controls['personId'].value}`);
-      }
-      else {
-        if (serverResponse.success)
-          this.onResetForm();
-          this.router.navigateByUrl('people');
-      }
-    }, 3000);
-  }
+	handleServerResponse(personS: Person) {
+		setTimeout(() => {
+			this.person = new Person();
+			if (this.updateForm) {
+				if (personS) this.router.navigateByUrl(`person/${this.controls['personId'].value}`);
+			} else {
+				if (personS) this.onResetForm();
+				this.router.navigateByUrl('people');
+			}
+		}, 3000);
+	}
 
-  onResetForm() {
-    this.personForm.reset();
-    this.invalidForm = false;
-  }
+	onResetForm() {
+		this.personForm.reset();
+		this.invalidForm = false;
+	}
 
-  calculatePulsations() {
-    if (this.controls['personAge'].valid && this.controls['personSex'].valid) {
-      const sex = this.controls['personSex'].value;
-      const age = +this.controls['personAge'].value;
-      this.personService.calculatePulsations(sex, age).subscribe(pul => this.pulsations = pul);
-      this.invalidForm = false;
-    }
-    else this.invalidForm = true;
-  }
+	calculatePulsations() {
+		if (this.controls['personAge'].valid && this.controls['personSex'].valid) {
+			const sex = this.controls['personSex'].value;
+			const age = +this.controls['personAge'].value;
+			this.personService.calculatePulsations(sex, age).subscribe((pul) => (this.pulsations = pul));
+			this.invalidForm = false;
+		} else this.invalidForm = true;
+	}
 }
--- ClientApp/src/app/services/person.service.ts
+++ ClientApp/src/app/services/person.service.ts
@@ -1,83 +1,86 @@
 import { Injectable } from '@angular/core';
-import { Person } from "../models/person";
-import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { Person } from '../models/person';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { HandleHttpErrorService } from '../@base/handle-http-error.service';
 import { Observable } from 'rxjs';
 import { tap, catchError } from 'rxjs/operators';
 import { ServerResponse } from '../@base/server-response';
 
 const httpOptions = {
-  headers: new HttpHeaders({
-    'Content-Type' : 'application/json',
-    'Authorization' : 'my-auth-token'  
-  })
+	headers: new HttpHeaders({
+		'Content-Type': 'application/json',
+		Authorization: 'my-auth-token'
+	})
 };
 
 @Injectable({
-  providedIn: 'root'
+	providedIn: 'root'
 })
 export class PersonService {
-  private peopleUrl: string = 'api/People';
+	private peopleUrl: string = 'api/People';
 
-  constructor(
-    private http: HttpClient,
-    private handleHttpError: HandleHttpErrorService
-  ) { }
+	constructor(private http: HttpClient, private handleHttpError: HandleHttpErrorService) {}
 
-  post(person: Person): Observable<ServerResponse> {
-    return this.http.post<ServerResponse>(`${this.peopleUrl}/Insert`, person, httpOptions)
-      .pipe(
-        tap(_ => this.handleHttpError.log('Data save.')),
-        catchError(this.handleHttpError.handleError<ServerResponse>('savePerson', null))
-      );
-  }
+	post(person: Person): Observable<Person> {
+		return this.http
+			.post<Person>(`${this.peopleUrl}`, person, httpOptions)
+			.pipe(
+				tap((_) => this.handleHttpError.log('Data save.')),
+				catchError(this.handleHttpError.handleError<Person>('savePerson', null))
+			);
+	}
 
-  get(): Observable<Person[]> {
-    return this.http.get<Person[]>(`${this.peopleUrl}/People`)
-      .pipe(
-        tap(_ => this.handleHttpError.log('Data receive.')),
-        catchError(this.handleHttpError.handleError<Person[]>('getPeople', []))
-      );
-  }
+	get(): Observable<Person[]> {
+		return this.http
+			.get<Person[]>(`${this.peopleUrl}`)
+			.pipe(
+				tap((_) => this.handleHttpError.log('Data receive.')),
+				catchError(this.handleHttpError.handleError<Person[]>('getPeople', []))
+			);
+	}
 
-  put(person: Person): Observable<ServerResponse> {
-    return this.http.put<ServerResponse>(`${this.peopleUrl}/Update/${person.personId}`, person, httpOptions)
-      .pipe(
-        tap(_ => this.handleHttpError.log('Data update.')),
-        catchError(this.handleHttpError.handleError<ServerResponse>('updatePerson', null))
-      );
-  }
+	put(person: Person): Observable<Person> {
+		return this.http
+			.put<Person>(`${this.peopleUrl}/${person.personId}`, person, httpOptions)
+			.pipe(
+				tap((_) => this.handleHttpError.log('Data update.')),
+				catchError(this.handleHttpError.handleError<Person>('updatePerson', null))
+			);
+	}
 
-  delete(id: string): Observable<ServerResponse> {
-    return this.http.delete<ServerResponse>(`${this.peopleUrl}/Delete/${id}`)
-      .pipe(
-        tap(_ => this.handleHttpError.log('Data delete.')),
-        catchError(this.handleHttpError.handleError<ServerResponse>('deletePerson', null))
-      );
-  }
+	delete(id: string): Observable<ServerResponse> {
+		return this.http
+			.delete<ServerResponse>(`${this.peopleUrl}/${id}`)
+			.pipe(
+				tap((_) => this.handleHttpError.log('Data delete.')),
+				catchError(this.handleHttpError.handleError<ServerResponse>('deletePerson', null))
+			);
+	}
 
-  getPerson(id: string): Observable<Person> {
-    return this.http.get<Person>(`${this.peopleUrl}/Person/${id}`)
-      .pipe(
-        tap(_ => this.handleHttpError.log('Data receive.')),
-        catchError(this.handleHttpError.handleError<Person>('searchPerson', null))
-      );
-  };
+	getPerson(id: string): Observable<Person> {
+		return this.http
+			.get<Person>(`${this.peopleUrl}/${id}`)
+			.pipe(
+				tap((_) => this.handleHttpError.log('Data receive.')),
+				catchError(this.handleHttpError.handleError<Person>('searchPerson', null))
+			);
+	}
 
-  searchPeople(term: string): Observable<Person[]> {
-    return this.http.get<Person[]>(`${this.peopleUrl}/SearchPeople?personId=${term}`)
-      .pipe(
-        tap(_ => this.handleHttpError.log('Data receive.')),
-        catchError(this.handleHttpError.handleError<Person[]>('searchPeople', []))
-      );
-  }
+	searchPeople(term: string): Observable<Person[]> {
+		return this.http
+			.get<Person[]>(`${this.peopleUrl}/SearchPeople?personId=${term}`)
+			.pipe(
+				tap((_) => this.handleHttpError.log('Data receive.')),
+				catchError(this.handleHttpError.handleError<Person[]>('searchPeople', []))
+			);
+	}
 
-  calculatePulsations(sex: string, age: number): Observable<number> {
-    return this.http.get<number>(`${this.peopleUrl}/CalculatePulsations?sex=${sex}&age=${age}`)
-      .pipe(
-        tap(_ => this.handleHttpError.log('Pulsations calculated')),
-        catchError(this.handleHttpError.handleError<number>('calculatePulsations', 0))
-      );
-  }
-
+	calculatePulsations(sex: string, age: number): Observable<number> {
+		return this.http
+			.get<number>(`${this.peopleUrl}/CalculatePulsations?sex=${sex}&age=${age}`)
+			.pipe(
+				tap((_) => this.handleHttpError.log('Pulsations calculated')),
+				catchError(this.handleHttpError.handleError<number>('calculatePulsations', 0))
+			);
+	}
 }
